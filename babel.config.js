caf2bf3a41cf062a6f02b5189e160701a1810660
igocooper <igocooper@gmail.com>
2023-05-31 21:07:54 +0200

    RA-8: add webpack

---
+++ babel.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        useBuiltIns: 'usage',
+        corejs: { version: 3, proposals: true },
+        bugfixes: true,
+      },
+    ],
+    '@babel/preset-typescript',
+    '@babel/preset-react',
+  ],
+  plugins: [['@babel/plugin-proposal-class-properties', { loose: true }]],
+  env: {
+    development: {
+      presets: [
+        [
+          '@babel/preset-react',
+          {
+            useBuiltIns: true,
+          },
+        ],
+      ],
+      plugins: ['react-refresh/babel'],
+    },
+    production: {
+      plugins: ['transform-react-remove-prop-types'],
+    },
+    test: {
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+      ],
+    },
+  },
+};
