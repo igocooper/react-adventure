4e54045d4fa61b8f6d6df9a126c5474cfb843388
igocooper <igocooper@gmail.com>
2023-06-12 21:23:08 +0200

    RA-11: add change turn logic

--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import { counterReducer } from 'modules/mainScreen/containers/counter/CounterSlice';
 import { battleFieldReducer } from 'modules/battleField/reducers';
 
 export const rootReducer = combineReducers({
-  counter: counterReducer,
   battleField: battleFieldReducer
 });
