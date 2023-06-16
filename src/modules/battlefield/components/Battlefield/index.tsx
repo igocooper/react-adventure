a9b8dea525c541f6f3fd89271cf8e0ab40f8d152
igocooper <igocooper@gmail.com>
2023-06-16 18:44:13 +0200

    RA-16: add highlighter

--- src/modules/battlefield/components/Battlefield/index.tsx
+++ src/modules/battlefield/components/Battlefield/index.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { CharacterContainer } from 'modules/battlefield/containers/CharacterContainer';
 import { HealthBar } from 'modules/battlefield/components/HealthBar';
 import type { Trooper } from '../../types';
-import { Location, Attackers, Defenders, Tile } from './styled';
+import { Location, Attackers, Defenders, Tile, Highlighter } from './styled';
 
 interface BattlefieldProps {
   attackers: Trooper[];
   defenders: Trooper[];
+  activeTrooper: Trooper;
   cursor: string;
+  hoveredTrooperId: number | undefined;
 }
 
 export const Battlefield = ({
   attackers,
   defenders,
-  cursor
+  cursor,
+  activeTrooper,
+  hoveredTrooperId
 }: BattlefieldProps) => {
   return (
     <Location $cursor={cursor}>
@@ -21,22 +25,46 @@ export const Battlefield = ({
       <div>
         <Attackers>
           {attackers.map(
-            ({ id, position, type, team, currentHealth, health }: Trooper) => (
-              <Tile key={id} $position={position}>
-                <HealthBar currentHealth={currentHealth} health={health} />
-                <CharacterContainer type={type} id={id} team={team} />
-              </Tile>
-            )
+            ({ id, position, type, team, currentHealth, health }: Trooper) => {
+              const active = id === activeTrooper?.id;
+              const hovered = hoveredTrooperId === id;
+              return (
+                <Tile key={id} $position={position}>
+                  {hovered && (
+                    <HealthBar currentHealth={currentHealth} health={health} />
+                  )}
+                  <CharacterContainer type={type} id={id} team={team} />
+                  {(active || hovered) && (
+                    <Highlighter
+                      $enemy={!active && team !== activeTrooper?.team}
+                      $team={team}
+                    />
+                  )}
+                </Tile>
+              );
+            }
           )}
         </Attackers>
         <Defenders>
           {defenders.map(
-            ({ id, position, type, team, currentHealth, health }: Trooper) => (
-              <Tile key={id} $position={position}>
-                <HealthBar currentHealth={currentHealth} health={health} />
-                <CharacterContainer type={type} id={id} team={team} />
-              </Tile>
-            )
+            ({ id, position, type, team, currentHealth, health }: Trooper) => {
+              const active = id === activeTrooper?.id;
+              const hovered = hoveredTrooperId === id;
+              return (
+                <Tile key={id} $position={position}>
+                  {hovered && (
+                    <HealthBar currentHealth={currentHealth} health={health} />
+                  )}
+                  <CharacterContainer type={type} id={id} team={team} />
+                  {(active || hovered) && (
+                    <Highlighter
+                      $enemy={!active && team !== activeTrooper?.team}
+                      $team={team}
+                    />
+                  )}
+                </Tile>
+              );
+            }
           )}
         </Defenders>
       </div>
