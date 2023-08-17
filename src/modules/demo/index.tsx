8f2274ca5f3b2d91a395ec3f93190043020e2f47
Oleg Lytvyn <oleg.lytvyn@pandadoc.com>
2023-08-17 20:17:11 +0200

    RA-90: Add rage effect visualisation

--- src/modules/demo/index.tsx
+++ src/modules/demo/index.tsx
@@ -1,6 +1,172 @@
-import React from 'react';
-import { Container } from './styled';
+import React, { useCallback, useState, useRef } from 'react';
+import { Container, Location, Hero } from './styled';
+import { useLocation } from 'common/hooks/useLocation';
+import { getCharacterProps } from '../battlefield/helpers/getCharacterProps';
+import { CHARACTER } from '../wardrobe/constants';
+import { DAMAGE_TYPE } from '../../common/constants';
+import { TROOPER_TEAM } from '../battlefield/constants';
+import {
+  CharacterAnimation,
+  OnLoadArgs
+} from '../animation/containers/CharacterAnimation';
+import { register } from '../animation/troopersAnimationInstances';
+import {
+  registerTrooperNode,
+  getTrooperNode
+} from '../battlefield/troopersNodesMap';
+import { useDispatch } from 'store/hooks';
+import { useKeyPress } from 'common/hooks/useKeyPress';
+import { wait } from 'common/helpers/wait';
 
-export const Demo = () => (
-  <Container>Put your component here for development</Container>
-);
+type Position = {
+  x: number;
+  y: number;
+};
+
+const STEP_X = 200;
+const STEP_Y = 50;
+const SPEED_X = 800;
+const SPEED_Y = 400;
+const SPEED_BASE = .6;
+
+export const Demo = () => {
+  const location = useLocation();
+  const characterRef = useRef<CharacterAnimation>(null);
+  const dispatch = useDispatch();
+  const [position, setPosition] = useState<Position>({
+    x: 0,
+    y: 400
+  });
+  const [forwardDirection, setDirection] = useState(true);
+  const timeRef = useRef(1000);
+
+  const handleLoad = useCallback(
+    ({ id, canvasNode, instance }: OnLoadArgs) => {
+      register(id, instance);
+      registerTrooperNode(id, canvasNode);
+    },
+    [dispatch]
+  );
+
+  const handleClick = async (event) => {
+    if (!characterRef.current) return;
+    characterRef.current.run();
+
+    const heroNode = getTrooperNode(CHARACTER.id);
+
+    const {
+      x: currentHeroX,
+      y: currentHeroY,
+      width,
+      height
+    } = heroNode!.getBoundingClientRect();
+
+    if (currentHeroX > event.clientX) {
+      setDirection(false);
+    } else {
+      setDirection(true);
+    }
+
+    const x = event.clientX - width / 2;
+    const y = event.clientY - height / 2;
+
+    const distance = Math.floor(
+      Math.sqrt(
+        Math.pow(Math.abs(currentHeroX - x), 2) +
+          Math.pow(Math.abs(currentHeroY - y), 2)
+      )
+    );
+
+    timeRef.current = distance / SPEED_BASE;
+
+    console.log('time', timeRef.current);
+    console.log('distance', distance);
+    console.log('speed', SPEED_BASE);
+
+    setPosition(() => ({
+      y,
+      x
+    }));
+
+    await wait(timeRef.current);
+    void characterRef.current.idle();
+  };
+
+  const onMoveRight = async () => {
+    if (!characterRef.current) return;
+    characterRef.current.run();
+    setPosition((state) => ({
+      y: state.y,
+      x: state.x + STEP_X
+    }));
+    setDirection(true);
+    await wait(SPEED_X);
+    void characterRef.current.idle();
+  };
+
+  const onMoveLeft = async () => {
+    if (!characterRef.current) return;
+    characterRef.current.run();
+    setPosition((state) => ({
+      y: state.y,
+      x: state.x - STEP_X
+    }));
+    setDirection(false);
+    await wait(SPEED_X);
+    void characterRef.current.idle();
+  };
+
+  const onMoveUp = async () => {
+    if (!characterRef.current) return;
+    characterRef.current.run();
+    setPosition((state) => ({
+      y: state.y - STEP_Y,
+      x: state.x
+    }));
+    await wait(SPEED_Y);
+    void characterRef.current.idle();
+  };
+
+  const onMoveDown = async () => {
+    if (!characterRef.current) return;
+    characterRef.current.run();
+    setPosition((state) => ({
+      y: state.y + STEP_Y,
+      x: state.x
+    }));
+    await wait(SPEED_Y);
+    void characterRef.current.idle();
+  };
+
+  useKeyPress('ArrowRight', onMoveRight);
+  useKeyPress('ArrowLeft', onMoveLeft);
+  useKeyPress('ArrowUp', onMoveUp);
+  useKeyPress('ArrowDown', onMoveDown);
+
+  return (
+    <Container>
+      <Location $location={location} onClick={handleClick}>
+        <Hero
+          $forwardDirection={forwardDirection}
+          // $speedX={SPEED_X}
+          // $speedY={SPEED_Y}
+          $time={timeRef.current}
+          $position={position}
+        >
+          <CharacterAnimation
+            ref={characterRef}
+            {...getCharacterProps({
+              type: CHARACTER.type,
+              equipment: CHARACTER.equipment,
+              appearance: CHARACTER.appearance,
+              damageType: DAMAGE_TYPE.PHYSICAL
+            })}
+            id={CHARACTER.id}
+            team={TROOPER_TEAM.ATTACKERS}
+            onLoad={handleLoad}
+          />
+        </Hero>
+      </Location>
+    </Container>
+  );
+};
--- src/modules/demo/styled.tsx
+++ src/modules/demo/styled.tsx
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import cursorDefaultImg from '../battlefield/images/cursors/cursor-default.png';
+import { LocationName } from '../battlefield/types';
 
 export const Container = styled.div`
   display: flex;
@@ -10,3 +12,44 @@ export const Container = styled.div`
 
   height: 100vh;
 `;
+
+type LocationProps = {
+  $location: LocationName;
+};
+
+export const Location = styled.div<LocationProps>`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: 0;
+  background-repeat: repeat;
+  display: flex;
+  justify-content: center;
+  transition: background-position 200ms linear;
+  background-image: url(${({ $location }) =>
+    `/images/locations/${$location}.png`});
+  cursor: url(${cursorDefaultImg}), auto;
+`;
+
+type HeroProps = {
+  $position: {
+    x: number;
+    y: number;
+  };
+  $speedX?: number;
+  $speedY?: number;
+  $forwardDirection: boolean;
+  $time: number;
+};
+export const Hero = styled.div<HeroProps>`
+  position: absolute;
+  top: ${({ $position }) => $position.y}px;
+  left: ${({ $position }) => $position.x}px;
+  transform: ${({ $forwardDirection }) =>
+    $forwardDirection ? 'initial' : 'rotate3d(0, 1, 0, 180deg)'};
+
+  transition: ${({ $time }) => `left ${$time}ms linear, top ${$time}ms linear`};
+`;
+
+// transition: ${({ $speedX, $speedY }) =>
+// `left ${$speedX}ms linear, top ${$speedY}ms linear`};
